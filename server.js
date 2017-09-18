fe752fe0cde03b99b33989fa4e46a63181e0f9ba
jossephalvarez <jossephalvarez@gmail.com>
2017-09-18 19:04:25 +0200

    second commit

--- server.js
+++ server.js
@@ -6,19 +6,23 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 
 app.use((req,res,next)=>{
   var now=new Date().toString();
   var log=`${now}:${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log',log+'\n');
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err){
+      console.log('Unable to append to server.log')
+    }
+  });
   next();
 });
 // app.use((req,res,next) => {
 //   res.render('maintence.hbs');
 // });
-app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
@@ -37,7 +41,8 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page'
+    pageTitle: 'About Page',
+    currentYear:new Date().getFullYear()
   });
 });
 
